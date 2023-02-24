3cdd3f3a7835a85f1668a55efd0d5c1a3973a0d6
yqzhishen <yangqian_1015@icloud.com>
2023-02-25 00:23:01 +0800

    Remove debug output

--- augmentation/spec_stretch.py
+++ augmentation/spec_stretch.py
@@ -39,11 +39,7 @@ class SpectrogramStretchAugmentation(BaseAugmentation):
             aug_item['sec'] /= aug_item['speed']
             aug_item['ph_durs'] /= aug_item['speed']
             aug_item['mel2ph'] = self.get_mel2ph(aug_item['ph_durs'], aug_item['len'])
-            try:
-                aug_item['f0'], aug_item['pitch'] = get_pitch_parselmouth(wav, mel, hparams, speed=speed)
-            except ValueError:
-                print(f'Error padding f0 in {aug_item["wav_fn"]}!')
-                print(key_shift, speed)
+            aug_item['f0'], aug_item['pitch'] = get_pitch_parselmouth(wav, mel, hparams, speed=speed)
 
         if key_shift != 0. or hparams.get('use_key_shift_embed', False):
             aug_item['key_shift'] = key_shift
