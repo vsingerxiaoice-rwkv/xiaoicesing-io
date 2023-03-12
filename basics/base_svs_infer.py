764a65b56c4464f620292222db68287fe606599a
yqzhishen <yangqian_1015@icloud.com>
2023-03-12 23:28:07 +0800

    Remove example_run

--- basics/base_svs_infer.py
+++ basics/base_svs_infer.py
@@ -6,7 +6,7 @@ import torch
 from pypinyin import lazy_pinyin
 
 from modules.vocoders.registry import VOCODERS
-from utils.hparams import set_hparams, hparams
+from utils.hparams import hparams
 from utils.phoneme_utils import build_g2p_dictionary, build_phoneme_list
 from utils.text_encoder import TokenTextEncoder
 
@@ -147,41 +147,3 @@ class BaseSVSInfer:
         output = self.forward_model(inp, return_mel=return_mel)
         output = self.postprocess_output(output)
         return output
-
-    @classmethod
-    def example_run(cls, inp, target='infer_out/example_out.wav'):
-        # settings hparams
-        set_hparams(print_hparams=False)
-
-        # call the model
-        infer_ins = cls(hparams)
-        out = infer_ins.infer_once(inp)
-
-        # output to file
-        os.makedirs(os.path.dirname(target), exist_ok=True)
-        print(f'| save audio: {target}')
-        from utils.audio import save_wav
-        save_wav(out, target, hparams['audio_sample_rate'])
-
-# if __name__ == '__main__':
-# debug
-# a = BaseSVSInfer(hparams)
-# a.preprocess_input({'text': '你 说 你 不 SP 懂 为 何 在 这 时 牵 手 AP',
-#                     'notes': 'D#4/Eb4 | D#4/Eb4 | D#4/Eb4 | D#4/Eb4 | rest | D#4/Eb4 | D4 | D4 | D4 | D#4/Eb4 | F4 | D#4/Eb4 | D4 | rest',
-#                     'notes_duration': '0.113740 | 0.329060 | 0.287950 | 0.133480 | 0.150900 | 0.484730 | 0.242010 | 0.180820 | 0.343570 | 0.152050 | 0.266720 | 0.280310 | 0.633300 | 0.444590'
-#                     })
-
-# b = {
-#     'text': '小酒窝长睫毛AP是你最美的记号',
-#     'notes': 'C#4/Db4 | F#4/Gb4 | G#4/Ab4 | A#4/Bb4 F#4/Gb4 | F#4/Gb4 C#4/Db4 | C#4/Db4 | rest | C#4/Db4 | A#4/Bb4 | G#4/Ab4 | A#4/Bb4 | G#4/Ab4 | F4 | C#4/Db4',
-#     'notes_duration': '0.407140 | 0.376190 | 0.242180 | 0.509550 0.183420 | 0.315400 0.235020 | 0.361660 | 0.223070 | 0.377270 | 0.340550 | 0.299620 | 0.344510 | 0.283770 | 0.323390 | 0.360340'
-# }
-# c = {
-#     'text': '小酒窝长睫毛AP是你最美的记号',
-#     'ph_seq': 'x iao j iu w o ch ang ang j ie ie m ao AP sh i n i z ui m ei d e j i h ao',
-#     'note_seq': 'C#4/Db4 C#4/Db4 F#4/Gb4 F#4/Gb4 G#4/Ab4 G#4/Ab4 A#4/Bb4 A#4/Bb4 F#4/Gb4 F#4/Gb4 F#4/Gb4 C#4/Db4 C#4/Db4 C#4/Db4 rest C#4/Db4 C#4/Db4 A#4/Bb4 A#4/Bb4 G#4/Ab4 G#4/Ab4 A#4/Bb4 A#4/Bb4 G#4/Ab4 G#4/Ab4 F4 F4 C#4/Db4 C#4/Db4',
-#     'note_dur_seq': '0.407140 0.407140 0.376190 0.376190 0.242180 0.242180 0.509550 0.509550 0.183420 0.315400 0.315400 0.235020 0.361660 0.361660 0.223070 0.377270 0.377270 0.340550 0.340550 0.299620 0.299620 0.344510 0.344510 0.283770 0.283770 0.323390 0.323390 0.360340 0.360340',
-#     'is_slur_seq': '0 0 0 0 0 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0'
-# }  # input like Opencpop dataset.
-# a.preprocess_input(b)
-# a.preprocess_input(c, input_type='phoneme')
