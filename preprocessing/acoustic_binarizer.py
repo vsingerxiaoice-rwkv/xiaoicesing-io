a39a9faa671b6a8665e3c276db8236b740f39a6d
hrukalive <bowen.parallife@gmail.com>
2023-03-24 22:35:43 -0500

    binarizer joint aug bug fix

--- preprocessing/acoustic_binarizer.py
+++ preprocessing/acoustic_binarizer.py
@@ -351,7 +351,7 @@ class AcousticBinarizer(BaseBinarizer):
                     aug_list.append(aug_task)
                 elif aug_type == 1:
                     aug_task = deepcopy(aug_item)
-                    aug_item['kwargs']['speed'] = speed
+                    aug_task['kwargs']['speed'] = speed
                     if aug_item['name'] in aug_map:
                         aug_map[aug_item['name']].append(aug_task)
                     else:
