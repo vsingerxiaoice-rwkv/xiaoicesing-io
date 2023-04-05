3b6e9ef47296f1e0ff82c07933caf7d67b8430fb
yqzhishen <yangqian_1015@icloud.com>
2023-04-05 17:08:22 +0800

    Fix for key mismatch

--- modules/toplevel.py
+++ modules/toplevel.py
@@ -16,8 +16,8 @@ class DiffSingerAcoustic(CategorizedModule):
             k_step=hparams['K_step'],
             denoiser_type=hparams['diff_decoder_type'],
             loss_type=hparams['diff_loss_type'],
-            norm_min=hparams['spec_min'],
-            norm_max=hparams['spec_max']
+            spec_min=hparams['spec_min'],
+            spec_max=hparams['spec_max']
         )
 
     @property
