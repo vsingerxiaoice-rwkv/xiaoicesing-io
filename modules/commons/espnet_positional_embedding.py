9a195b8e9274ab2bc23e86f915d24437bfd48452
yqzhishen <yangqian_1015@icloud.com>
2023-04-05 22:14:22 +0800

    Revert and fix `PositionalEncoding`

--- modules/commons/espnet_positional_embedding.py
+++ modules/commons/espnet_positional_embedding.py
@@ -1,5 +1,4 @@
 import math
-
 import torch
 
 
@@ -8,31 +7,42 @@ class PositionalEncoding(torch.nn.Module):
     Args:
         d_model (int): Embedding dimension.
         dropout_rate (float): Dropout rate.
+        max_len (int): Maximum input length.
         reverse (bool): Whether to reverse the input position.
     """
 
-    def __init__(self, d_model, dropout_rate, reverse=False):
+    def __init__(self, d_model, dropout_rate, max_len=5000, reverse=False):
         """Construct an PositionalEncoding object."""
         super(PositionalEncoding, self).__init__()
         self.d_model = d_model
         self.reverse = reverse
         self.xscale = math.sqrt(self.d_model)
         self.dropout = torch.nn.Dropout(p=dropout_rate)
+        self.pe = None
+        self.extend_pe(torch.tensor(0.0).expand(1, max_len))
 
-    def generate_pe(self, length, device):
-        """Generate the positional encodings."""
-        position = torch.arange(0, length, 1., device=device).unsqueeze(1)
+    def extend_pe(self, x):
+        """Reset the positional encodings."""
+        if self.pe is not None:
+            if self.pe.size(1) >= x.size(1):
+                if self.pe.dtype != x.dtype or self.pe.device != x.device:
+                    self.pe = self.pe.to(dtype=x.dtype, device=x.device)
+                return
         if self.reverse:
-            position = position.flip(0)
+            position = torch.arange(
+                x.size(1) - 1, -1, -1.0, dtype=torch.float32
+            ).unsqueeze(1)
+        else:
+            position = torch.arange(0, x.size(1), dtype=torch.float32).unsqueeze(1)
         div_term = torch.exp(
-            torch.arange(0., self.d_model, 2., device=device)
+            torch.arange(0, self.d_model, 2, dtype=torch.float32)
             * -(math.log(10000.0) / self.d_model)
         )
         pe = torch.stack([
             torch.sin(position * div_term),
             torch.cos(position * div_term)
         ], dim=2).view(-1, self.d_model).unsqueeze(0)
-        return pe
+        self.pe = pe.to(device=x.device, dtype=x.dtype)
 
     def forward(self, x: torch.Tensor):
         """Add positional encoding.
@@ -41,7 +51,8 @@ class PositionalEncoding(torch.nn.Module):
         Returns:
             torch.Tensor: Encoded tensor (batch, time, `*`).
         """
-        x = x * self.xscale + self.generate_pe(x.size(1), x.device)
+        self.extend_pe(x)
+        x = x * self.xscale + self.pe[:, : x.size(1)]
         return self.dropout(x)
 
 
@@ -51,11 +62,12 @@ class ScaledPositionalEncoding(PositionalEncoding):
     Args:
         d_model (int): Embedding dimension.
         dropout_rate (float): Dropout rate.
+        max_len (int): Maximum input length.
     """
 
-    def __init__(self, d_model, dropout_rate):
+    def __init__(self, d_model, dropout_rate, max_len=5000):
         """Initialize class."""
-        super().__init__(d_model=d_model, dropout_rate=dropout_rate)
+        super().__init__(d_model=d_model, dropout_rate=dropout_rate, max_len=max_len)
         self.alpha = torch.nn.Parameter(torch.tensor(1.0))
 
     def reset_parameters(self):
@@ -69,7 +81,8 @@ class ScaledPositionalEncoding(PositionalEncoding):
         Returns:
             torch.Tensor: Encoded tensor (batch, time, `*`).
         """
-        x = x + self.alpha * self.generate_pe(x.size(1), x.device)
+        self.extend_pe(x)
+        x = x + self.alpha * self.pe[:, : x.size(1)]
         return self.dropout(x)
 
 
@@ -79,11 +92,12 @@ class RelPositionalEncoding(PositionalEncoding):
     Args:
         d_model (int): Embedding dimension.
         dropout_rate (float): Dropout rate.
+        max_len (int): Maximum input length.
     """
 
-    def __init__(self, d_model, dropout_rate):
+    def __init__(self, d_model, dropout_rate, max_len=5000):
         """Initialize class."""
-        super().__init__(d_model, dropout_rate, reverse=True)
+        super().__init__(d_model, dropout_rate, max_len, reverse=True)
 
     def forward(self, x):
         """Compute positional encoding.
@@ -93,6 +107,7 @@ class RelPositionalEncoding(PositionalEncoding):
             torch.Tensor: Encoded tensor (batch, time, `*`).
             torch.Tensor: Positional embedding tensor (1, time, `*`).
         """
+        self.extend_pe(x)
         x = x * self.xscale
-        pos_emb = self.generate_pe(x.size(1), x.device)
+        pos_emb = self.pe[:, : x.size(1)]
         return self.dropout(x) + self.dropout(pos_emb)
