b02fb5d870986a3acd80f5d1e38f3d414167db41
yqzhishen <yangqian_1015@icloud.com>
2023-04-13 19:47:23 +0800

    Add docstring and transpose

--- modules/losses/diff_loss.py
+++ modules/losses/diff_loss.py
@@ -14,8 +14,13 @@ class DiffusionNoiseLoss(nn.Module):
             raise NotImplementedError()
 
     def forward(self, x_recon: Tensor, noise: Tensor, nonpadding: Tensor = None) -> Tensor:
+        """
+        :param x_recon: [B, 1, M, T]
+        :param noise: [B, 1, M, T]
+        :param nonpadding: [B, T, M]
+        """
         if nonpadding is not None:
-            nonpadding = nonpadding.unsqueeze(1)
+            nonpadding = nonpadding.transpose(1, 2).unsqueeze(1)
             x_recon *= nonpadding
             noise *= nonpadding
         return self.loss(x_recon, noise)
