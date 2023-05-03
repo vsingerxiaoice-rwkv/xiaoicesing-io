483fbcb9c2abdfd27f76950785ee8a4ff19c0f0d
yqzhishen <yangqian_1015@icloud.com>
2023-05-04 01:08:46 +0800

    Support augmentation with energy and breathiness

--- augmentation/spec_stretch.py
+++ augmentation/spec_stretch.py
@@ -8,6 +8,9 @@ from modules.fastspeech.tts_modules import LengthRegulator
 from modules.vocoders.registry import VOCODERS
 from utils.binarizer_utils import get_pitch_parselmouth, get_mel2ph_torch
 from utils.hparams import hparams
+from utils.infer_utils import resample_align_curve
+
+VARIANCE_CHECKLIST = ['energy', 'breathiness']
 
 
 class SpectrogramStretchAugmentation(BaseAugmentation):
@@ -22,12 +25,6 @@ class SpectrogramStretchAugmentation(BaseAugmentation):
 
     @require_same_keys
     def process_item(self, item: dict, key_shift=0., speed=1., replace_spk_id=None) -> dict:
-        if 'energy' in item:
-            raise NotImplementedError('Energy has not been supported in augmentation.')
-
-        if 'breathiness' in item:
-            raise NotImplementedError('Breathiness has not been supported in augmentation.')
-
         aug_item = deepcopy(item)
         if hparams['vocoder'] in VOCODERS:
             wav, mel = VOCODERS[hparams['vocoder']].wav2spec(
@@ -48,10 +45,40 @@ class SpectrogramStretchAugmentation(BaseAugmentation):
             aug_item['mel2ph'] = get_mel2ph_torch(
                 self.lr, torch.from_numpy(aug_item['ph_dur']), aug_item['length'], self.timestep, device=self.device
             ).cpu().numpy()
+
             f0, _ = get_pitch_parselmouth(
                 wav, aug_item['length'], hparams, speed=speed, interp_uv=hparams['interp_uv']
             )
             aug_item['f0'] = f0.astype(np.float32)
+
+            # NOTE: variance curves are directly resampled according to speed,
+            # despite how frequency-domain features change after the augmentation.
+            # For acoustic models, this can bring more (but not much) difficulty
+            # to learn how variance curves affect the mel spectrograms, since
+            # they must realize how the augmentation causes the mismatch.
+            #
+            # This is a simple way to combine augmentation and variances. However,
+            # dealing variance curves like this will decrease the accuracy of
+            # variance controls. In most situations, not being ~100% accurate
+            # will not ruin the user experience. For example, it does not matter
+            # if the energy does not exactly equal the RMS; it is just fine
+            # as long as higher energy can bring higher loudness and strength.
+            # The neural networks itself cannot be 100% accurate, though.
+            #
+            # There are yet other choices to simulate variance curves:
+            #   1. Re-extract the features from resampled waveforms;
+            #   2. Re-extract the features from re-constructed waveforms using
+            #      the transformed mel spectrograms through the vocoder.
+            # But there are actually no perfect ways to make them all accurate
+            # and stable.
+            for v_name in VARIANCE_CHECKLIST:
+                if v_name in item:
+                    aug_item[v_name] = resample_align_curve(
+                        aug_item[v_name],
+                        original_timestep=self.timestep,
+                        target_timestep=self.timestep * aug_item['speed'],
+                        align_length=aug_item['length']
+                    )
 
         if key_shift != 0. or hparams.get('use_key_shift_embed', False):
             if replace_spk_id is None:
