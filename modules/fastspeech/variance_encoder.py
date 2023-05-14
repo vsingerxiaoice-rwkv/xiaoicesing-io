fe53b22bd0dddeeb482a4c1e4b7973d1300e0e54
yqzhishen <yangqian_1015@icloud.com>
2023-05-14 22:38:44 +0800

    Directly use `ph_dur_embed` if predict_dur is false

--- modules/fastspeech/variance_encoder.py
+++ modules/fastspeech/variance_encoder.py
@@ -14,10 +14,15 @@ from utils.text_encoder import PAD_INDEX
 class FastSpeech2Variance(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
+        self.predict_dur = hparams['predict_dur']
+
         self.txt_embed = Embedding(vocab_size, hparams['hidden_size'], PAD_INDEX)
-        self.midi_embed = Embedding(128, hparams['hidden_size'])
-        self.onset_embed = Embedding(2, hparams['hidden_size'])
-        self.word_dur_embed = Linear(1, hparams['hidden_size'])
+
+        if self.predict_dur:
+            self.onset_embed = Embedding(2, hparams['hidden_size'])
+            self.word_dur_embed = Linear(1, hparams['hidden_size'])
+        else:
+            self.ph_dur_embed = Linear(1, hparams['hidden_size'])
 
         if hparams['use_spk_id']:
             self.spk_embed = Embedding(hparams['num_spk'], hparams['hidden_size'])
@@ -28,7 +33,8 @@ class FastSpeech2Variance(nn.Module):
         )
 
         dur_hparams = hparams['dur_prediction_args']
-        if hparams['predict_dur']:
+        if self.predict_dur:
+            self.midi_embed = Embedding(128, hparams['hidden_size'])
             self.dur_predictor = DurationPredictor(
                 in_dims=hparams['hidden_size'],
                 n_chans=dur_hparams['hidden_size'],
@@ -45,27 +51,32 @@ class FastSpeech2Variance(nn.Module):
         :param txt_tokens: (train, infer) [B, T_ph]
         :param midi: (train, infer) [B, T_ph]
         :param ph2word: (train, infer) [B, T_ph]
-        :param ph_dur: (train) [B, T_ph]
+        :param ph_dur: (train, [infer]) [B, T_ph]
         :param word_dur: (infer) [B, T_w]
         :param infer: whether inference
         :return: encoder_out, ph_dur_pred
         """
-        b = txt_tokens.shape[0]
-        onset = torch.diff(ph2word, dim=1, prepend=ph2word.new_zeros(b, 1)) > 0
-        onset_embed = self.onset_embed(onset.long())  # [B, T_ph, H]
-        if word_dur is None or not infer:
-            word_dur = ph_dur.new_zeros(b, ph2word.max() + 1).scatter_add(
-                1, ph2word, ph_dur
-            )[:, 1:]  # [B, T_ph] => [B, T_w]
-        word_dur = torch.gather(F.pad(word_dur, [1, 0], value=0), 1, ph2word)  # [B, T_w] => [B, T_ph]
-        word_dur_embed = self.word_dur_embed(word_dur.float()[:, :, None])
-        encoder_out = self.encoder(txt_tokens, onset_embed + word_dur_embed)
+        if self.predict_dur:
+            b = txt_tokens.shape[0]
+            onset = torch.diff(ph2word, dim=1, prepend=ph2word.new_zeros(b, 1)) > 0
+            onset_embed = self.onset_embed(onset.long())  # [B, T_ph, H]
 
-        if not hparams['predict_dur']:
+            if word_dur is None or not infer:
+                word_dur = ph_dur.new_zeros(b, ph2word.max() + 1).scatter_add(
+                    1, ph2word, ph_dur
+                )[:, 1:]  # [B, T_ph] => [B, T_w]
+            word_dur = torch.gather(F.pad(word_dur, [1, 0], value=0), 1, ph2word)  # [B, T_w] => [B, T_ph]
+            word_dur_embed = self.word_dur_embed(word_dur.float()[:, :, None])
+
+            encoder_out = self.encoder(txt_tokens, onset_embed + word_dur_embed)
+
+            midi_embed = self.midi_embed(midi)  # => [B, T_ph, H]
+            dur_cond = encoder_out + midi_embed
+            ph_dur_pred = self.dur_predictor(dur_cond, x_masks=txt_tokens == PAD_INDEX, infer=infer)
+
+            return encoder_out, ph_dur_pred
+        else:
+            ph_dur_embed = self.ph_dur_embed(ph_dur.float()[:, :, None])
+            encoder_out = self.encoder(txt_tokens, ph_dur_embed)
+
             return encoder_out, None
-
-        midi_embed = self.midi_embed(midi)  # => [B, T_ph, H]
-        dur_cond = encoder_out + midi_embed
-        ph_dur_pred = self.dur_predictor(dur_cond, x_masks=txt_tokens == PAD_INDEX, infer=infer)
-
-        return encoder_out, ph_dur_pred
